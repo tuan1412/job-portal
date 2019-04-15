46b5e0675b3a99a8e5ae6005b8c98872110344c1
danght <danght@hblab.vn>
2019-04-16 00:47:50 +0700

    Create middleware CheckRole

--- cn-web-backend/laravel/routes/api.php
+++ cn-web-backend/laravel/routes/api.php
@@ -35,34 +35,38 @@ Route::group([
 //     Route::resourse('users', 'Admin\UserController');
 // }
 
-Route::group([
-    'namespace'  => 'CandidateUser',
-    'prefix'     => 'candidate_user',
-], function () {
-    Route::get('find_job', 'JobController@index');
-    Route::get('find_job_advance', 'JobController@indexAdvance');
-
-    Route::get('get_all_cv', 'CVController@getAll');
-    Route::get('get_cv/{id}', 'CVController@index');
-    Route::post('create_cv', 'CVController@create');
-    Route::post('update_cv/{id}', 'CVController@update');
-    Route::delete('delete_cv/{id}', 'CVController@delete');
-    Route::post('apply_cv', 'CVController@apply');
-});
-
-Route::group([
-    'namespace'  => 'CompanyUser',
-    'prefix'     => 'company_user',
-], function () {
-    Route::get('get_users', 'UserController@index');
-    Route::post('create_user/{company_id}', 'UserController@create');
-    Route::post('update_user/{user_id}', 'UserController@update');
-    Route::delete('delete_user/{user_id}', 'UserController@delete');
-
-    Route::post('create_job/{company_id}', 'JobController@create');
-    Route::get('get_jobs', 'JobController@index');
-    Route::post('update_job/{id}', 'JobController@update');
-    Route::delete('delete_job/{id}', 'JobController@delete');
-
-    Route::get('get_cv/{company_id}', 'CVController@index');
+Route::group(['middleware' => ['jwt.auth']], function () {
+    Route::group([
+        'namespace'  => 'CandidateUser',
+        'middleware' => 'role:candidate_user',
+        'prefix'     => 'candidate_user',
+    ], function () {
+        Route::get('find_job', 'JobController@index');
+        Route::get('find_job_advance', 'JobController@indexAdvance');
+    
+        Route::get('get_all_cv', 'CVController@getAll');
+        Route::get('get_cv/{id}', 'CVController@index');
+        Route::post('create_cv', 'CVController@create');
+        Route::post('update_cv/{id}', 'CVController@update');
+        Route::delete('delete_cv/{id}', 'CVController@delete');
+        Route::post('apply_cv', 'CVController@apply');
+    });
+    
+    Route::group([
+        'namespace'  => 'CompanyUser',
+        'middleware' => 'role:company_user',
+        'prefix'     => 'company_user',
+    ], function () {
+        Route::get('get_users', 'UserController@index');
+        Route::post('create_user/{company_id}', 'UserController@create');
+        Route::post('update_user/{user_id}', 'UserController@update');
+        Route::delete('delete_user/{user_id}', 'UserController@delete');
+    
+        Route::post('create_job/{company_id}', 'JobController@create');
+        Route::get('get_jobs', 'JobController@index');
+        Route::post('update_job/{id}', 'JobController@update');
+        Route::delete('delete_job/{id}', 'JobController@delete');
+    
+        Route::get('get_cv/{company_id}', 'CVController@index');
+    });
 });
