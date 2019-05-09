3d53c622cd26bd24d101004c195fd01c969c0c52
Dang JR <dungmt10697@gmail.com>
2019-05-09 17:37:02 +0700

    Fix code

--- cn-web-backend/laravel/app/Http/Controllers/CompanyUser/CVController.php
+++ cn-web-backend/laravel/app/Http/Controllers/CompanyUser/CVController.php
@@ -10,11 +10,12 @@ class CVController extends Controller
 {
     public function index(Request $request)
     {
-        return DB::table('cvs')
+        return  DB::table('cvs')
 		        	->join('cv_job', 'cv_job.cv_id', '=', 'cvs.id')
+		        	->join('candidate_users', 'candidate_users.user_id', '=', 'cvs.user_id')
 		        	->where('cv_job.job_id', $request->job_id)
-		        	->select('name', 'path')
-		        	->orderBy('cvs.created_at', 'desc')
+		        	->select('name', 'path', 'cvs.user_id', 'full_name')
+					->orderBy('cvs.created_at', 'desc')
 		        	->paginate($request->per_page);
     }
 }
